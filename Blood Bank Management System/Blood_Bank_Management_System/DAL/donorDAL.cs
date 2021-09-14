8c11bc1d6e3f6ea31e4e6c7719429e9c77e0b966
MD SHAKIL KHAN <77512256+mdshakilkhan841@users.noreply.github.com>
2021-09-14 17:22:26 +0530

    Add files via upload

---
+++ Blood Bank Management System/Blood_Bank_Management_System/DAL/donorDAL.cs
@@ -0,0 +1,124 @@
+﻿using Blood_Bank_Management_System.BLL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Blood_Bank_Management_System.DAL
+{
+    class donorDAL
+    {
+        //Create a connection string to connect Database
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString; 
+        #region SELECT to display data ion DataGridView from database
+        public DataTable select()
+        {
+            //Create object to DataTable to hold the data from datatable and return it
+            DataTable dt = new DataTable();
+
+            //Create object of SQL connection to connect database 
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                //Write the SQL query to select the dta from database
+                string sql = "SELECT * FROM tbl_donors";
+
+                //Create the SQLCommand tp Execute the Query
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Create SQL data adapter to hold the data Temporarily
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                //Open Databse Connection
+                conn.Open();
+
+                //Pass the data from adapter to database
+                adapter.Fill(dt);
+
+            }
+            catch(Exception ex)
+            {
+                //Display message if there is any exceptional errors
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //Close Database connection
+                conn.Close();
+            }
+
+            return dt;
+        }
+        #endregion
+
+        #region INSERT Data to database
+        public bool Insert(donorBLL d)
+        {
+            //Create a boolean variable and set its default value to false
+            bool isSucess = false;
+
+            //Ctrate SQL connection to connect database
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                //Write the data Query to INSERT data into database
+                string sql = "INSERT INTO tbl_donors (first_name, last_name, email, contact, gender, address, blood_group, added_date, image_name, added_by) VALUES (@first_name, @last_name, @email, @contact, @gender, @address, @blood_group, @added_date, @image_name, @added_by)";
+
+                //Create SQL command to exceute the query
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Pass the value to SQL Query
+                cmd.Parameters.AddWithValue("@first_name", d.first_name);
+                cmd.Parameters.AddWithValue("@last_name", d.last_name);
+                cmd.Parameters.AddWithValue("@email", d.email);
+                cmd.Parameters.AddWithValue("@contact", d.contact);
+                cmd.Parameters.AddWithValue("@gender", d.gender);
+                cmd.Parameters.AddWithValue("@address", d.address);
+                cmd.Parameters.AddWithValue("@blood_group", d.blood_group);
+                cmd.Parameters.AddWithValue("@added_date", d.added_date);
+                cmd.Parameters.AddWithValue("@image_name", d.image_name);
+                cmd.Parameters.AddWithValue("@added_by", d.added_by);
+
+                //Open Dtabase Connection
+                conn.Open();
+
+                //Create an Inter variable to check  wheather the query was exceuted succcessfully or Not
+                int rows = cmd.ExecuteNonQuery();
+
+                //If the query is Exceuted successfully the value of rows will be greater than zero else it will be zero
+                if(rows>0)
+                {
+                    //Query Exceuted Successfully 
+                    isSucess = true;
+                }
+                else
+                {
+                    //Failed to Exceute Query
+                    isSucess = false;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                //Display Error Message if there's any exceptional Errors
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //Close Database Connection
+                conn.Close();
+            }
+
+            return isSucess;
+        }
+        #endregion
+    }
+}
---
+++ Blood Bank Management System/Blood_Bank_Management_System/DAL/userDAL.cs
@@ -0,0 +1,292 @@
+﻿using Blood_Bank_Management_System.BLL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Blood_Bank_Management_System.DAL
+{
+    class userDAL
+    {
+        //Create a Static string to connet database
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
+        #region SELECT data fron database 
+        public DataTable Select()
+        {
+            //Create an iobject to connect database
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            //Create Datatable to Hold the data from database
+            DataTable dt = new DataTable();
+
+            try
+            {
+                //Write SQL quary to get tha data from batabase
+                String sql = "SELECT * FROM tbl_users";
+
+                //Create SQL command to execute query
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Create Sql Data adapter to hold data from the database temporaily
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                //Open Database connection 
+                conn.Open();
+
+                // Transfer Data from SqlData adapter to data table 
+                adapter.Fill(dt);
+            
+            }
+            catch(Exception ex)
+            {
+                //Display error message if there is any exceptional errors
+                MessageBox.Show(ex.Message); 
+            }
+            finally
+            {
+                //Close database Connection
+                conn.Close();
+            }
+
+            return dt;
+        }
+        #endregion
+
+        #region Insert Data into Database for User Module
+        public bool Insert(userBLL u)
+        {
+            //Create boolean variable and set its default value to false
+            bool isSuccess = false;
+
+            //Create an object of Sqlconnection to connect database
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                //Create a string variable to store the INSERT query
+                string sql = "INSERT INTO tbl_users(username, email, password, full_name, contact, address, added_date, image_name) VALUES(@username, @email, @password, @full_name, @contact, @address, @added_date, @image_name)";
+
+                //Create sql Command to pass the value in our query
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Create the parameter to pass get the value from UI and pass it on SQL query above
+                cmd.Parameters.AddWithValue("@username", u.username);
+                cmd.Parameters.AddWithValue("@email", u.email);
+                cmd.Parameters.AddWithValue("@password", u.password);
+                cmd.Parameters.AddWithValue("@full_name", u.full_name);
+                cmd.Parameters.AddWithValue("@contact", u.contact);
+                cmd.Parameters.AddWithValue("@address", u.address);
+                cmd.Parameters.AddWithValue("@added_date", u.added_date);
+                cmd.Parameters.AddWithValue("@image_name", u.image_name);
+
+                //Open Datebase connection 
+                conn.Open();
+
+                //Create an integer variable to hold the value after the query is execuited
+                int rows = cmd.ExecuteNonQuery();
+
+                //The value of rows will begreater than 0 if the query is executed successfully
+                //Else it'll be 0
+
+                if (rows > 0)
+                {
+                    //Query Executed successfully 
+                    isSuccess = true;
+                }
+                else
+                {
+                    //Failled to Execute query 
+                    isSuccess = false;
+                }
+
+            }
+            catch(Exception ex)
+            {
+                //Display Error message if there is any exceptional errors 
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //Close database Connection
+                conn.Close();
+            }
+
+
+            return isSuccess;
+        }
+        #endregion
+
+        #region UPDATE data in  database(User Module)
+        public bool Update(userBLL u)
+        {
+            //create a boolean variable and set its default value to false
+            bool isSuccess = false;
+
+            //CreateParams an object for database connection
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+
+            try
+            {
+                //create a string variable to hold the SQL query
+                string sql = "UPDATE tbl_users SET username=@username, email=@email, password=@password, full_name=@full_name, contact=@contact, address=@address, added_date=@added_date, image_name=@image_name WHERE user_id=@user_id";
+
+                //Create sql command to execute querty and also pass the values to sql query 
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Now pass the values to SQL query 
+                cmd.Parameters.AddWithValue("@username", u.username);
+                cmd.Parameters.AddWithValue("@email", u.email);
+                cmd.Parameters.AddWithValue("@password", u.password);
+                cmd.Parameters.AddWithValue("@full_name", u.full_name);
+                cmd.Parameters.AddWithValue("@contact", u.contact);
+                cmd.Parameters.AddWithValue("@address", u.address);
+                cmd.Parameters.AddWithValue("@added_date", u.added_date);
+                cmd.Parameters.AddWithValue("@image_name", u.image_name);
+                cmd.Parameters.AddWithValue("@user_id", u.user_id);
+
+                //Open Database Connection
+                conn.Open();
+
+                //Create an integer variable to hold the value after the query is execuited
+                int rows = cmd.ExecuteNonQuery();
+
+
+                //The value of rows will begreater than 0 if the query is executed successfully
+                //Else it'll be 0
+
+                if (rows > 0)
+                {
+                    //Query Executed successfully 
+                    isSuccess = true;
+                }
+                else
+                {
+                    //Failled to Execute query 
+                    isSuccess = false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //Display error message if there is any exceptional errors
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //Close database Connection
+                conn.Close();
+            }
+
+            return isSuccess;
+
+        }
+        #endregion
+
+        #region DELETE data from database(User Module)
+        public bool Delete(userBLL u)
+        {
+            //create a boolean variable and set its default value to false
+            bool isSuccess = false;
+
+            //CreateParams an object for sql connection
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+
+            try
+            {
+                //create a string variable to delete the SQL data
+                string sql = "DELETE FROM tbl_users WHERE user_id=@user_id";
+
+                //Create sql command to execute querty
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Now pass the values to parameters 
+                cmd.Parameters.AddWithValue("@user_id", u.user_id);
+
+                //Open The Database Connection
+                conn.Open();
+
+                //Create an integer variable to hold the value after the query is execuited
+                int rows = cmd.ExecuteNonQuery();
+
+
+                //The value of rows will begreater than 0 if the query is executed successfully
+                //Else it'll be 0
+
+                if (rows > 0)
+                {
+                    //Query Executed successfully 
+                    isSuccess = true;
+                }
+                else
+                {
+                    //Failled to Execute query 
+                    isSuccess = false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //Display error message if there is any exceptional errors
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //Close database Connection
+                conn.Close();
+            }
+
+            return isSuccess;
+
+        }
+        #endregion
+
+        #region SEARCH
+        public DataTable Search(string keywords)
+        {
+            //1. Create an SQL connection to connect Database
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            //2. Create Datqa Table to hold the data from database temporarilly
+            DataTable dt = new DataTable();
+            try
+            {
+
+                string sql = "SELECT * FROM tbl_users WHERE user_id LIKE '%" + keywords + "%' OR full_name LIKE '%" + keywords + "%' OR address LIKE '%" + keywords + "%'";
+
+
+                //Create sql command to execute querty
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Create SQL Data adapter to get the data from database
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                //Open The Database Connection
+                conn.Open();
+
+                //Pass the data from adapter to datatable
+                adapter.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                //Display error message if there is any exceptional errors
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //Close database Connection
+                conn.Close();
+            }
+            return dt;
+        }
+        #endregion
+
+    }
+}
